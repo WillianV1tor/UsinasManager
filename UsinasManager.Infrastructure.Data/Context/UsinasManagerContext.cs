54ae8266d166bf692a6ad5f1da934b80d75db809
WillianV1tor <willianv1tor.dev@gmail.com>
2021-07-29 22:54:10 -0300

    Estrutura do projeto

---
+++ UsinasManager.Infrastructure.Data/Context/UsinasManagerContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsinasManager.Infrastructure.Data.Context
+{
+	public class UsinasManagerContext : DbContext
+	{
+		public UsinasManagerContext() : base("UsinasManagerDB")
+		{
+
+		}
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+		}
+	}
+}
